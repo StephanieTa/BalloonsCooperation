0b896a717f88a95b722575ef84b54c508bc0b28a
Stephanie Ta <stephanie.ta@outlook.com>
2014-01-18 17:08:46 +0100

    Create AirPumpView

---
+++ BalloonsCooperation/AirPumpView.h
@@ -0,0 +1,24 @@
+//
+//  AirPumpView.h
+//  BalloonsCooperation
+//
+//  Created by Stephanie Ta on 18.01.14.
+//  Copyright (c) 2014 Stephanie Ta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@protocol AirPumpViewDelegate;
+
+@interface AirPumpView : UIView
+
+@property (nonatomic, weak) id <AirPumpViewDelegate> delegate;
+
+@end
+
+@protocol AirPumpViewDelegate <NSObject>
+
+- (void)didTapOnAirPump:(UIView *)airPumpView;
+
+@end
