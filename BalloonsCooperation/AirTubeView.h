546a84a1bf194262e261566b712ad323afce46ac
Stephanie Ta <stephanie.ta@outlook.com>
2014-01-18 17:00:09 +0100

    Create AirTube classes

---
+++ BalloonsCooperation/AirTubeView.h
@@ -0,0 +1,16 @@
+//
+//  AirTubeView.h
+//  BalloonsCooperation
+//
+//  Created by Stephanie Ta on 18.01.14.
+//  Copyright (c) 2014 Stephanie Ta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AirTubeView : UIView
+
+- (void)drawAirTubeAtPosition:(NSString *)position;
+- (void)animateIdeaAlongAirTubeAtPosition:(NSString *)position completion:(void (^)(BOOL finished))completionBlock;
+
+@end
---
+++ BalloonsCooperation/Glyphs/UIBezierPath+AirTubeGlyph.h
@@ -0,0 +1,17 @@
+//
+//  UIBezierPath+AirTubeGlyph.h
+//  BalloonsCooperation
+//
+//  Created by Stephanie Ta on 18.01.14.
+//  Copyright (c) 2014 Stephanie Ta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIBezierPath (AirTubeGlyph)
+
++ (instancetype)airTubeGlyphLeft;
++ (instancetype)airTubeGlyphCenter;
++ (instancetype)airTubeGlyphRight;
+
+@end
