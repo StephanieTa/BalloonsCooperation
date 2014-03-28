43c7b94777dda34fba0b4e6bd60b54990bb6da2b
Stephanie Ta <stephanie.ta@outlook.com>
2014-03-28 13:55:16 +0100

    forgot a note

--- BalloonsCooperation/ViewController.m
+++ BalloonsCooperation/ViewController.m
@@ -263,8 +263,8 @@
      NSString *path = [docsDirectory stringByAppendingPathComponent:@"dataLoggingFromDevice_cooperate.txt"];
      */
     
-//    /* Comment this out when you start the app on the simulator, CHANGE "van" in path to your username!!!
-    NSString *path = @"/Users/van/Desktop/dataLoggingFromSimulator_cooperate.txt";
+//    /* Comment this out when you start the app on the simulator, CHANGE "yourUsername" in path to your username!!!
+    NSString *path = @"/Users/yourUsername/Desktop/dataLoggingFromSimulator_cooperate.txt";
 //    */
     
     if (![[NSFileManager defaultManager] fileExistsAtPath:path]){
